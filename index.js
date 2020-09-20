950536b4f12b80d7c864a795a7a4adb34e2d8423
Blessing <bntoko@gmail.com>
2020-09-20 14:58:31 +0200

    added classes to store changing values and their respective objects

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ let polySides = 6;
 let currentTool = 'brush';
 let canvasWidth = 600;
 let canvasHeight = 600;
+
+class ShapeBoundingBox {
+    constructor(left, top, width, height) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class MouseDownPos {
+    constructor(x, y) {
+        this.x = x;
+        this.y= y;
+    }
+}
+
+class Location {
+    constructor(x, y) {
+        this.x = x;
+        this.y= y; 
+    }
+}
+
+class PolyPoint {
+    constructor(x, y) {
+        this.x = x;
+        this.y= y; 
+    }
+}
+
+let shapeBoundingBox = new ShapeBoundingBox(0,0,0,0);
+let mouseDownPos = new MouseDownPos(0,0);
+let location = new Location(0,0);
+let polyPoint = new PolyPoint(0,0);
